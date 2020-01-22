6b6000c5106712146da2b10ceaf09bf2638e389e
SanusiMD <just4deen@yahoo.com>
2020-01-22 19:49:33 +0100

    Intial comit

---
+++ src/Die.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Die.css";
+
+class Die extends Component {
+  render() {
+    return (
+  <i className={` Die-icon fas fa-dice-${this.props.face}`}></i>
+      
+    );
+  }
+}
+
+export default Die;
